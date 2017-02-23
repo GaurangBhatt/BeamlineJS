c5264d8c2071ea0a71ab3c1168cebff19b03dd95
Gaurang Bhatt <gaurang.bhatt@capitalone.com>
2017-02-22 21:17:09 -0500

    renamed forkline

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 # Build pipeline-manager lambda function
-cd pipeline-manager
-npm run all
-
 cd ../notification-line
 npm run all
 
-cd ../fork-line
+cd pipeline-manager
+npm run all
+
+cd ../beamline
 npm run all
