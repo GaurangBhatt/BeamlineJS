dec4caddacde3a3948a5e3dfbe9bf61591b078b9
Gaurang Bhatt <gaurang.bhatt@capitalone.com>
2017-03-05 01:29:42 -0500

    enabled npm run all

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 # Build pipeline-manager lambda function
 cd notification-line
-#npm run all
+npm run all
 
 cd ../pipeline-manager
-#npm run all
+npm run all
 
 cd ../beamline
-#npm run all
+npm run all
 
 cd ../
 source ./setup.properties
